fb987f24cbe3b8c4a3b50e2bac0ae9acf4a4eddd
Herb Stahl <ghstahl@gmail.com>
2019-03-13 06:28:54 -0700

    initial

---
+++ src/SignalRHostApp/Hubs/SignalRUserProvider.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRHostApp.Hubs
+{
+    public class SignalRUserProvider : IUserIdProvider
+    {
+        public string GetUserId(HubConnectionContext connection)
+        {
+            var subject = (from claim in connection.User.Claims
+                where claim.Type == ClaimTypes.NameIdentifier
+                select claim.Value).FirstOrDefault();
+            return subject;
+        }
+    }
+}
